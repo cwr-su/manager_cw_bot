3b99c9145c66570e583dab1ecc20d9753a47ff52
Alexander Laptev <alex@cwr.su>
2024-10-13 17:52:02 +0300

    The logging system and format has been changed. Changes were made to the main-structure (function-file)

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ import sys
 from manager_cw_bot_api.business import run
 
 
+# Do not break the structure of func-s, classes, and components to make the modules work correctly.
 async def main() -> None:
     """Main Function (run bot)."""
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format='%(levelname)s: %(asctime)s -- %(funcName)s -- %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S'
+    )
+
     await run()
 
 
